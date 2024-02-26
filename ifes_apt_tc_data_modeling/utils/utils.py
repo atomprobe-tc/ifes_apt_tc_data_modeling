499639cfc1544d8d05fa503db25d545938116c97
mkuehbach <markus.kuehbach@physik.hu-berlin.de>
2024-02-26 09:24:36 +0100

    Moved remaining duplicate code from paraprobe-toolbox such that the paraprobe-toolbox can from now on just use this library instead of duplicating again function definitions

--- ifes_apt_tc_data_modeling/utils/utils.py
+++ ifes_apt_tc_data_modeling/utils/utils.py
@@ -19,6 +19,7 @@
 """Utilities for working with molecular ions in atom probe microscopy."""
 
 from typing import Tuple
+import re
 import numpy as np
 
 from ase.data import atomic_numbers, chemical_symbols
@@ -162,3 +163,106 @@ def is_range_significant(left: np.float64, right: np.float64) -> bool:
         if (right - left) > MQ_EPSILON:
             return True
     return False
+
+
+def is_convertible_to_isotope_hash(symbol: str):
+    """Check if human_readable symbol is convertible into nuclide hash tribool."""
+    case = re.search("^([A-Z])([a-z])?(-)([0-9]+)$", symbol)
+    if case is None:  # eventually element case e.g. "K"
+        if not isinstance(symbol, str):
+            raise ValueError("Argument symbol needs to be a string !")
+        if symbol not in get_smart_chemical_symbols():
+            raise ValueError(f"Symbol needs to be in {get_smart_chemical_symbols()}!")
+        return 1
+    # alternative case eventually specific nuclide e.g. "K-40"
+    symb_mass = symbol.split("-")
+    if len(symb_mass) != 2:
+        raise TypeError("Argument symbol is not properly formatted <symbol>-<mass_number>!")
+    if len(symb_mass[0]) != 1 and len(symb_mass[0]) != 2:
+        raise ValueError("Argument symbol is not properly formatted <symbol>-<mass_number>!")
+    if len(symb_mass[1]) <= 0:
+        raise ValueError(f"Argument symbol {symb_mass[1]} needs to be a physical mass number!")
+    if symb_mass[0] not in get_smart_chemical_symbols():
+        raise ValueError(f"{symb_mass[0]} is not a symbol in {get_smart_chemical_symbols()}!")
+    if int(symb_mass[1]) not in isotopes[atomic_numbers[symb_mass[0]]].keys():
+        raise ValueError(f"No value for isotopes[atomic_numbers[{symb_mass[0]}][{int(symb_mass[1])}] exists!")
+    return 2
+
+
+def element_or_nuclide_to_hash(symbol: str):
+    """Converts an element symbol (e.g. K) or nuclide (K-40) to nuclide hash."""
+    # consider moving this to the ifes_apt_tc_data_modeling library
+    case = is_convertible_to_isotope_hash(symbol)
+    if case == 1:
+        return isotope_to_hash(atomic_numbers[symbol], 0)
+    if case == 2:
+        symb_mass = symbol.split("-")
+        return isotope_to_hash(atomic_numbers[symb_mass[0]],
+                               int(symb_mass[1]) - atomic_numbers[symb_mass[0]])
+    return isotope_to_hash(0, 0)
+
+
+def symbol_lst_to_matrix_of_nuclide_vector(method: str = "resolve_all",
+                                           symbol_lst: list = [],
+                                           **kwargs):
+    """Parse human-readable element/isotope names to paraprobe."""
+    # method = "resolve_all"
+    # method = "resolve_unknown"
+    # method = "resolve_element"
+    # method = "resolve_isotope"
+    # method = "resolve_ion"
+    # symbol_lst = [["K", "C"], ["U-238", "H-2"]]
+    # symbol_lst = [["K-40", "C-14"], ["U-238", "H-2"]]  # one list per mol. ion
+    # kwargs_charge_lst = [+1, +3]  # one charge per ion
+    # resolve_all and resolve_unknown are the special cases
+    # where we do not need to specify a matrix of isotope vectors
+    if method in ("resolve_all", "resolve_unknown"):
+        return (None, None, None)
+
+    supported = ("resolve_element", "resolve_ion", "resolve_isotope")
+    if method not in supported:
+        raise ValueError(f"Argument method needs to be in {supported} !")
+    if not isinstance(symbol_lst, list) and all(isinstance(lst, list) for lst in symbol_lst):
+        raise TypeError("Argument symbol_lst must be a list of lists!")
+    if not all(len(np.shape(lst)) == 1 for lst in symbol_lst):
+        "One list in argument symbol_lst is not a 1d list or an empty list!"
+    if not all(np.shape(lst)[0] >= 1 for lst in symbol_lst):
+        "One list in argument symbol_lst is not a 1d list or an empty list!"
+    matrix = np.zeros([len(symbol_lst), MAX_NUMBER_OF_ATOMS_PER_ION])
+    charge = []
+    if (method == "resolve_ion") and ("charge_lst" in kwargs):
+        if not isinstance(kwargs["charge_lst"], list):
+            raise TypeError("Keyword argument charge_lst must be a list of lists!")
+        if not all(isinstance(val, int) for val in kwargs["charge_lst"]):
+            raise ValueError("Keyword argument charge_lst needs to be a list of int !")
+        if np.shape(symbol_lst)[0] != np.shape(kwargs["charge_lst"])[0]:
+            raise ValueError("Argument symbol_lst and keyword argument charge_lst need to have the same length !")
+    for idx, lst in enumerate(symbol_lst):
+        ivec = np.zeros([1, MAX_NUMBER_OF_ATOMS_PER_ION])
+        if lst == []:
+            raise ValueError("Argument molecular ion must not be an empty list!")
+        if len(lst) > MAX_NUMBER_OF_ATOMS_PER_ION:
+            raise ValueError(f"Argument molecular ion must not contain more than "
+                             f"{MAX_NUMBER_OF_ATOMS_PER_ION} entries!")
+        jdx = 0
+        for symbol in lst:
+            if method == "resolve_element":
+                if symbol in atomic_numbers:
+                    ivec[0, jdx] = isotope_to_hash(atomic_numbers[symbol], 0)  # do not encode isotope information
+                    jdx += 1
+                else:
+                    # it might be that we have a specific nuclide e.g. K-40 try to parse element symbol
+                    candidate = symbol.split("-", 1)[0]
+                    if candidate not in atomic_numbers:
+                        raise KeyError(f"symbol_lst[{idx}] candidate does not specify an element!")
+                    ivec[0, jdx] = isotope_to_hash(atomic_numbers[candidate], 0)
+                    jdx += 1
+            else:  # "resolve_isotope", "resolve_ion":
+                ivec[0, jdx] = element_or_nuclide_to_hash(symbol)
+                jdx += 1
+        matrix[idx, :] = ivec[0, :]
+        if method == "resolve_ion":
+            charge.append(kwargs["charge_lst"][idx])
+    if charge == []:
+        return (method, np.asarray(matrix, np.uint16), None)
+    return (method, np.asarray(matrix, np.uint16), np.asarray(charge, np.int8))
