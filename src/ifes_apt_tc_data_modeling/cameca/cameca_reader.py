8a897d1d9d609257d3846def6cfff87c37308c58
mkuehbach <markus.kuehbach@physik.hu-berlin.de>
2025-12-03 10:54:36 +0100

    Refactored prototype implementation into proper parser

---
+++ src/ifes_apt_tc_data_modeling/cameca/cameca_reader.py
@@ -0,0 +1,116 @@
+#
+# Copyright The NOMAD Authors.
+#
+# This file is part of NOMAD. See https://nomad-lab.eu for further info.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+"""Reader for Cameca HDF5 files as shared here https://doi.org/10.18126/dqxb-9m77."""
+
+# pylint: disable=fixme
+
+import os
+import h5py
+import re
+import numpy as np
+
+from ifes_apt_tc_data_modeling.nexus.nx_ion import NxIon
+from ifes_apt_tc_data_modeling.cameca.cameca_utils import parse_elements
+from ifes_apt_tc_data_modeling.utils.pint_custom_unit_registry import ureg
+from ifes_apt_tc_data_modeling.utils.utils import create_nuclide_hash
+
+from ifes_apt_tc_data_modeling.utils.custom_logging import logger
+
+
+class ReadCamecaHfiveFileFormat:
+    """Read Cameca HDF5 files used in this study https://doi.org/10.18126/dqxb-9m77."""
+
+    def __init__(self, file_path: str):
+        if not file_path.lower().endswith((".hdf5")):
+            raise ImportError(
+                "WARNING::HDF5 file incorrect file_path ending or file type."
+            )
+        self.file_path = file_path
+        self.file_size = os.path.getsize(self.file_path)
+        self.number_of_events = None
+        self.supported = 0  # voting-based
+        with h5py.File(self.file_path, "r") as h5r:
+            for entry in ("mass", "xyz", "ranges"):
+                if entry in h5r.keys():
+                    self.supported += 1
+            self.number_of_events = np.shape(h5r["mass"][...])[0]
+            if (
+                self.number_of_events == 0
+                or self.number_of_events != np.shape(h5r["xyz"][...])[0]
+            ):
+                logger.warning(
+                    f"{self.file_path} length of mass and xyz arrays is zero or differs."
+                )
+                self.supported = 0
+                return
+        if self.supported == 3:
+            logger.debug(f"{self.file_path} is a supported Cameca HDF5 file.")
+        else:
+            logger.warning(f"{self.file_path} is not a supported Cameca HDF5 file.")
+
+    def get_reconstructed_positions(self):
+        """Read xyz positions."""
+        values = np.zeros((self.number_of_events,), np.float32)
+        with h5py.File(self.file_path, "r") as h5r:
+            values[:, :] = np.asarray(h5r["mass"][...], np.float32)
+        return ureg.Quantity(values, ureg.nanometer)
+
+    def get_mass_to_charge_state_ratio(self):
+        """Read (calibrated?) mass-to-charge-state-ratio."""
+        values = np.zeros((self.number_of_events,), np.float32)
+        with h5py.File(self.file_path, "r") as h5r:
+            values[:] = np.asarray(h5r["mass"][...], np.float32)
+        return ureg.Quantity(values, ureg.dalton)
+
+    def get_ranging_definitions(self):
+        """Read ranging definitions."""
+        self.rng: dict = {"ranges": {}, "ions": {}, "molecular_ions": []}
+
+        with h5py.File(self.file_path, "r") as h5r:
+            for entry in h5r[
+                "ranges"
+            ]:  # clarify if reports following the creation order from Cameca or differently?
+                print(entry)
+                match = re.compile(r"^range_\d+$", entry)
+                if match and all(
+                    attribute_name in h5r["ranges"][entry].attrs
+                    for attribute_name in ["element", "min_da", "max_da"]
+                ):  # , "volume"]:
+                    ion_id = int(entry.replace("range_", ""))
+                    atoms = parse_elements(
+                        f"""{h5r["ranges"][entry].attrs["element"]}"""
+                    )
+                    min_mass_to_charge = np.float64(
+                        h5r["ranges"][entry].attrs["min_da"][()]
+                    )  # in examples was already f64
+                    max_mass_to_charge = np.float64(
+                        h5r["ranges"][entry].attrs["max_da"][()]
+                    )  # in examples was already f64
+
+                    m_ion = NxIon(
+                        nuclide_hash=create_nuclide_hash(atoms), charge_state=0
+                    )
+                    m_ion.comment = (
+                        entry  # for debugging above-mentioned issue if order retained
+                    )
+                    m_ion.add_range(min_mass_to_charge, max_mass_to_charge)
+                    m_ion.apply_combinatorics()
+
+                    self.rng["molecular_ions"].append(m_ion)
+        logger.info(f"{self.file_path} ranging definitions parsed successfully.")
---
+++ src/ifes_apt_tc_data_modeling/cameca/cameca_utils.py
@@ -0,0 +1,107 @@
+#
+# Copyright The NOMAD Authors.
+#
+# This file is part of NOMAD. See https://nomad-lab.eu for further info.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+import re
+from ifes_apt_tc_data_modeling.utils.molecular_ions import get_chemical_symbols
+
+
+def get_all_elements_with_given_number_of_characters(
+    number_characters: int = 0,
+) -> list:
+    selection = []
+    for symbol in get_chemical_symbols():
+        if len(symbol) == number_characters:
+            selection.append(symbol)
+    return selection
+
+
+def parse_elements(element: str, verbose: bool = False) -> list[str]:
+    parsed = []
+    elements_with_one_character_symbol = (
+        get_all_elements_with_given_number_of_characters(1)
+    )
+    elements_with_two_character_symbol = (
+        get_all_elements_with_given_number_of_characters(2)
+    )
+    idx = 0
+    while idx < len(element):
+        if verbose:
+            print(f"......{idx}")
+        if f"{element[idx : idx + 2]}" in elements_with_two_character_symbol:
+            if verbose:
+                print(f">>>>, 2 {element[idx : idx + 2]}")
+            match = re.search(r"^[0-9]+", element[idx + 2 : :])
+            if match:
+                if verbose:
+                    print(f"two_symbol {match}")
+                parsed += [element[idx : idx + 2]] * int(match.group(0))
+                if verbose:
+                    print(f"parsed, 2, ye match {parsed}, {match.group(0)}")
+                idx += 1 + len(f"{match.group(0)}")
+            else:
+                parsed.append(f"{element[idx : idx + 2]}")
+                if verbose:
+                    print(f"parsed, 2, no match{parsed}")
+                idx += 2
+        elif f"{element[idx]}" in elements_with_one_character_symbol:
+            if verbose:
+                print(f">>>>, 1 {element[idx]}")
+            match = re.search(r"^[0-9]+", element[idx + 1 : :])
+            if match:
+                if verbose:
+                    print(f"one_symbol {match}")
+                parsed += [f"{element[idx]}"] * int(match.group(0))
+                if verbose:
+                    print(f"parsed {parsed}, {match.group(0)}")
+                idx += 1 + len(f"{match.group(0)}")
+            else:
+                if idx + 2 < len(element):
+                    if (
+                        f"{element[idx : idx + 2]}"
+                        in elements_with_two_character_symbol
+                    ):
+                        if verbose:
+                            print(f">>>> {element[idx]}")
+                        match = re.search(r"^[0-9]+", element[idx + 2 : :])
+                        if match:
+                            if verbose:
+                                print(f"two_symbol {match}")
+                            parsed += [f"{element[idx]}"] * int(match.group(0))
+                            if verbose:
+                                print(f"parsed {parsed}, {match.group(0)}")
+                            idx += 2 + len(f"{match.group(0)}")
+                        else:
+                            parsed.append(f"{element[idx : idx + 2]}")
+                            if verbose:
+                                print(f"parsed {parsed}")
+                            idx += 2
+                    else:
+                        parsed.append(f"{element[idx]}")
+                        if verbose:
+                            print(f"parsed {parsed}")
+                        idx += 1
+                else:
+                    parsed.append(f"{element[idx]}")
+                    if verbose:
+                        print(f"parsed {parsed}")
+                    idx += 1
+        else:
+            if verbose:
+                print(f">>>>>>>>>>>{element[idx]}")
+            idx += 1
+    return parsed
