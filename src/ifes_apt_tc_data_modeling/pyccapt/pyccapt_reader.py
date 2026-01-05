d9b2aa1012b5bc838f60a518fb13ffd37ffd4f08
mkuehbach <markus.kuehbach@physik.hu-berlin.de>
2026-01-05 10:00:07 +0100

    explicit casting

--- src/ifes_apt_tc_data_modeling/pyccapt/pyccapt_reader.py
+++ src/ifes_apt_tc_data_modeling/pyccapt/pyccapt_reader.py
@@ -223,7 +223,8 @@ class ReadPyccaptRangingFileFormat:
         self.rng: dict[str, Any] = {}
         self.rng["molecular_ions"] = []
         logger.debug(np.shape(self.df)[0])
-        for idx in range(np.shape(self.df)[0]):
+        for idx in np.arange(0, np.shape(self.df)[0]):
+            idx = int(idx)
             if isinstance(self.df.iat[idx, 6], str):
                 if self.df.iat[idx, 6] == "unranged":
                     continue
